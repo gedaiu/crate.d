6141b8e5e3bc55c6e848edbde32096e9a0b9add6
Szabo Bogdan <szabobogdan@yahoo.com>
2014-11-27 23:48:10 +0200

    Added basic calendar item prototype

---
+++ source/crated/model/calendar.d
@@ -0,0 +1,64 @@
+﻿/**
+ * Authors: Szabo Bogdan <szabobogdan@yahoo.com>
+ * Date: 11 27, 2014
+ * License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+ * Copyright: Public Domain
+ */
+module crated.model.calendar;
+
+import std.exception;
+import std.datetime;
+
+/**
+ * Basic functionality for a calendar event
+ */
+class CalendarEventPrototype
+{
+
+	///Event start date
+	@("field") 
+	SysTime startDate;
+
+	///Event end date
+	@("field")
+	SysTime endDate;
+
+	///Invariant to check the event consistency
+	invariant() {
+		assert(startDate <= endDate, "`startDate` <= `endDate`");
+	}
+}
+
+unittest {
+	auto testEvent = new CalendarEventPrototype;
+
+	testEvent.startDate = Clock.currTime;
+	testEvent.endDate = Clock.currTime - dur!"hours"(1);
+
+	bool failed = false;
+
+	try {
+		assert(testEvent);
+	} catch (core.exception.AssertError e) {
+		failed = true;
+	}
+
+	assert(failed);
+}
+
+unittest {
+	auto testEvent = new CalendarEventPrototype;
+	
+	testEvent.startDate = Clock.currTime;
+	testEvent.endDate = Clock.currTime + dur!"hours"(1);
+	
+	bool failed = false;
+	
+	try {
+		assert(testEvent);
+	} catch (core.exception.AssertError e) {
+		failed = true;
+	}
+	
+	assert(!failed);
+}
