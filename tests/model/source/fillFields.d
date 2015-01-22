7a98940d1e79a53f5a072f69fb29b4f05f025a74
Szabo Bogdan <szabobogdan@yahoo.com>
2015-01-22 08:57:11 +0200

    Fixed tests

--- tests/model/source/fillFields.d
+++ tests/model/source/fillFields.d
@@ -120,3 +120,45 @@ unittest {
 	assert( bItem["names"]["first"].length == 1 );
 	assert( bItem["names"]["first"][0]["other"].get!string == "value" );
 }
+
+
+
+class SomeItem {
+	
+	@("field", "primary")
+	int id;
+	
+	@("field")
+	string[] names;
+	
+	@("field")
+	string[string] otherData;
+}
+
+alias Descriptor = ModelDescriptor!SomeItem;
+
+unittest {
+	
+	string[string] data;
+	
+	data["id"] = "0";
+	
+	data["names[0]"] = "name1";
+	data["names[1]"] = "name2";
+	
+	data["otherData[key1]"] = "val1";
+	data["otherData[key2]"] = "val2";
+	
+	auto item = Descriptor.CreateItem("", data);
+	
+	assert(item.id == 0);
+	
+	assert(item.names.length == 2);
+	assert(item.names[0] == "name1");
+	assert(item.names[1] == "name2");
+	
+	
+	assert(item.otherData["key1"] == "val1");
+	assert(item.otherData["key2"] == "val2");
+}
+
